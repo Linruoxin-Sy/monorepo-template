b01a863e3124acd8a39b2599ae8399ac8608cff0
Linruoxin-Sy <342972612@qq.com>
2026-04-22 00:47:19 +0800

    chore(root): change vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
+
+const createProject = (root: string) => ({
+  extends: true as const,
+  test: {
+    include: [`${root}/**/tests/**/*.{test,spec}.ts`],
+    alias: {
+      '@': new URL(`./${root}/src`, import.meta.url).pathname,
+    },
+  },
+});
 
 export default defineConfig({
   plugins: [vue()],
   test: {
-    projects: [
-      {
-        test: {
-          globals: true,
-          include: ['packages/**/tests/**/*.test.ts'],
-          browser: {
-            enabled: true,
-            provider: playwright(),
-            // https://vitest.dev/config/browser/playwright
-            instances: [{ browser: 'chromium' }],
-          },
-          alias: {
-            '@': fileURLToPath(new URL('./packages/shared/src', import.meta.url)),
-          },
-        },
-      },
-    ],
+    globals: true,
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      // https://vitest.dev/config/browser/playwright
+      instances: [{ browser: 'chromium' }],
+    },
+    projects: [createProject('apps/web'), createProject('packages/shared')],
   },
 });
